b114d12783afe5aeb14d61f4771c8b9a047ab5b4
Ray <218554081+rblez@users.noreply.github.com>
2026-06-01 11:09:19 -0400

    debug: add file logging for startup diagnostics

--- rs/src-tauri/src/main.rs
+++ rs/src-tauri/src/main.rs
@@ -1,5 +1,11 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
 fn main() {
-    memlink_gui_lib::run()
+    let log_path = std::env::temp_dir().join("memlink-gui.log");
+    let _ = std::fs::write(&log_path, "memlink-gui starting...\n");
+
+    std::panic::set_hook(Box::new(move |info| {
+        let msg = format!("panic: {info}\n");
+        let _ = std::fs::write(&log_path, &msg);
+    }));
+
+    memlink_gui_lib::run();
 }
